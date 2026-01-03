c2c670e6a42ea3129b50058b7c69ced98e01e351
unknown <jemish.jikadara2007@gmail.com>
2026-01-03 19:12:25 +0530

    first added

---
+++ typing.js
@@ -0,0 +1,192 @@
+const esy = 'in one good real one not school set they state high life consider on and not come what also for set point can want as while with of order child about school thing never hold find order each too between program work end you home place around while place problem end begin interest while public or where see time those increase interest be give end think seem small as both another a child same eye you between way do who into again good fact than under very head become real possible some write know however late each that with because that place nation only for each change form consider we would interest with world so order or run more open that large write turn never over open each over change still old take hold need give by consider line only leave while what set up number part form want against great problem can because head so first this here would course become help year first end want both fact public long word down also long for without new turn against the because write seem line interest call not if line thing what work people way may old consider leave hold want life between most place may if go who need fact such program where which end off child down change to from people high during people find to however into small new general it do that could old for last get another hand much eye great no work and with but good there last think can around use like number never since world need what we around part show new come seem while some and since still small these you general which seem will place come order form how about just also they with state late use both early too lead general seem there point take general seem few out like might under if ask while such interest feel word right again how about system such between late want fact up problem stand new say move a lead small however large public out by eye here over so be way use like say people work for since interest so face order school good not most run problem group run she late other problem real form what just high no man do under would to each too end point give number child through so this large see get form also all those course to work during about he plan still so like down he look down where course at who plan way so since come against he all who at world because while so few last these mean take house who old way large no first too now off would in this course present order home public school back own little about he develop of do over help day house stand present another by few come that down last or use say take would each even govern play around back under some line think she even when from do real problem between long as there school do as mean to all on other good may from might call world thing life turn of he look last problem after get show want need thing old other during be again develop come from consider the now number say life interest to system only group world same state school one problem between for turn run at very against eye must go both still all a as so after play eye little be those should out after which these both much house become both school this he real and may mean time by real number other as feel at end ask plan come turn by all head increase he present increase use stand after see order lead than system here ask in of look point little too without each for both but right we come world much own set we right off long those stand go both but under now must real general then before with much those at no of we only back these person plan from run new as own take early just increase only look open follow get that on system the mean plan man over it possible if most late line would first without real hand say turn point small set at in system however to be home show new again come under because about show face child know person large program how over could thing from out world while nation stand part run have look what many system order some one program you great could write day do he any also where child late face eye run still again on by as call high the must by late little mean never another seem to leave because for day against public long number word about after much need open change also'.split(' ');
+
+const normal = 'Learning Is A Continuous Process That Never Truly Ends In A Person Life Because Every Day Brings New Experiences New Challenges And New Opportunities To Grow When A Person Decides To Learn Something New They Open The Door To Knowledge Confidence And Self Improvement Education Plays A Very Important Role In Shaping A Person Thinking Skills And Behavior Because It Helps Them Understand The World In A Better Way A Curious Mind Always Seeks Answers And Never Stops Asking Questions Which Is The First Step Toward True Learning When Students Focus On Their Studies With Dedication And Discipline They Slowly Build A Strong Foundation That Helps Them Achieve Their Goals In The Future Learning Is Not Limited To Books Or Classrooms It Can Happen Anywhere At Any Time Through Observation Practice And Real Life Experiences A Person Who Is Willing To Learn From Mistakes Grows Faster Than Someone Who Is Afraid Of Failure Because Mistakes Teach Valuable Lessons That No Book Can Fully Explain Hard Work Patience And Consistency Are The Key Elements Of Success In Any Learning Journey Because Results Do Not Come Overnight But Slowly Through Regular Effort When A Student Practices Daily They Improve Their Skills And Gain Confidence In Their Abilities Which Motivates Them To Keep Going Education Also Helps A Person Develop Critical Thinking Skills That Allow Them To Analyze Situations And Make Better Decisions In Life A Well Educated Person Is More Likely To Contribute Positively To Society By Using Their Knowledge To Help Others And Solve Problems Learning New Skills Such As Technology Communication And Problem Solving Is Very Important In The Modern World Because The World Is Constantly Changing And Demanding New Abilities People Who Adapt And Learn New Things Stay Ahead And Create Better Opportunities For Themselves Reading Books Improves Vocabulary Imagination And Understanding While Writing Helps Express Thoughts Clearly And Effectively Listening To Others With Attention Teaches Empathy And Broadens Perspective Which Is An Important Part Of Personal Growth A Student Who Balances Studies With Practical Learning Gains Real World Knowledge That Is Useful Beyond Exams Time Management Is Another Important Skill That Students Learn During Their Educational Journey Because It Helps Them Balance Study Work And Personal Life A Disciplined Routine Makes Learning Easier And Reduces Stress Which Improves Overall Performance Teachers Play A Very Important Role In Guiding Students And Inspiring Them To Believe In Their Potential Because A Good Teacher Not Only Teaches Subjects But Also Values And Confidence Self Belief Is Very Powerful Because When A Person Believes In Themselves They Are More Likely To Take Action And Face Challenges With Courage Learning Also Improves Creativity Because It Encourages The Mind To Think In New And Innovative Ways Curiosity Drives Innovation And Innovation Leads To Progress Which Benefits Individuals And Society As A Whole Technology Has Made Learning More Accessible Than Ever Before Because Information Is Available At The Click Of A Button Online Resources Videos And Interactive Platforms Allow Students To Learn At Their Own Pace And According To Their Interests However Self Discipline Is Very Important In Digital Learning Because Distractions Are Also Easily Available A Focused Mind Can Turn Any Resource Into A Powerful Learning Tool Education Teaches Respect Responsibility And Teamwork Which Are Essential Qualities For Personal And Professional Life Group Activities And Discussions Help Students Learn Cooperation And Communication Skills Which Prepare Them For Future Careers A Positive Attitude Toward Learning Makes Difficult Subjects Easier And Reduces Fear Because A Willing Mind Is Always Ready To Accept Challenges Success Is Not Just About Talent But About Effort Consistency And The Desire To Improve Every Day People Who Keep Learning Throughout Their Lives Stay Mentally Active And Emotionally Strong Which Helps Them Handle Life Challenges Better Learning Gives Purpose Direction And Confidence Which Are Necessary For A Meaningful Life A Person Who Understands The Value Of Education Never Takes Knowledge For Granted And Always Tries To Improve Themselves By Learning Something New Each Day In The End Learning Is Not Just About Achieving Success Or Earning Money But About Becoming A Better More Aware And More Responsible Human Being Who Can Make A Positive Difference In The World'.split(' ');
+
+ const hard = 'Learning coding in 2025 Is not Just a Trend but a Powerful Skill that helps students build a Better Future because technology Is Everywhere and almost Every Industry Uses Software tools Apps Websites And Digital Systems When a student starts learning Programming languages like HTML Css JavaScript Python Java Or C++ they often feel confused at First but with time practice and consistency everything becomes Easier Step by Step Coding requires logic patience creativity and focus which helps improve problem solving skills in real life for example if a student practices 30 minutes daily for 10 days they can clearly see improvement in their speed accuracy and confidence Numbers like 10 20 30 50 and 100 are often used to measure progress while percentages like 75% 85% and 95% show performance in exams projects or tasks Symbols such as @ # $ % ^ & * ( ) _ + = ! ? / | < > are very important in programming and password creation because they add security and complexity A strong password example can be Learn@Code2025#Success! which includes Uppercase lowercase numbers and special characters making it hard to crack Education is not only about scoring high marks like 90 or 100 but about understanding concepts clearly and applying them in real life situations When students learn consistently they develop discipline time management and responsibility which are very important qualities for success in school college and professional life Technology has made learning easier because information is available online 24x7 through videos articles blogs ebooks and tutorials Platforms like websites apps and online courses help students learn at their own pace but distractions like social media games and notifications can reduce focus so self control is very important A focused student can turn even 15 minutes into productive learning time Mistakes are a natural part of the learning process and every error teaches a valuable lesson for example a syntax error missing semicolon ; or wrong bracket { } can break code but fixing it improves understanding Debugging errors improves patience and logical thinking Communication skills are also important because a good developer must explain ideas clearly to teammates clients and users Using proper uppercase lowercase punctuation commas periods and symbols makes communication effective Learning English math science and computer skills together creates a strong foundation for future growth Hard work consistency and a positive mindset always beat talent without effort because success is built slowly day by day Students who practice daily even when they feel tired or bored develop mental strength and confidence Confidence helps reduce fear of exams interviews and challenges Life is not always easy and problems will come but a person who knows how to learn adapt and improve can handle any situation Coding also improves creativity because it allows people to build websites games apps and tools from scratch Turning an idea into reality using code is very satisfying and motivating For example creating a simple calculator using numbers like 1 2 3 4 5 and operators like + - * / teaches both logic and mathematics Learning never stops because even professionals with 10 15 or 20 years of experience keep updating their skills due to new technologies frameworks and tools Growth mindset means believing that skills can be improved with effort and practice rather than thinking I cannot do this Education teaches values like honesty teamwork respect and responsibility which help build good character Group projects help students learn collaboration while individual practice improves self reliance Reading books improves vocabulary imagination and understanding while writing improves expression and clarity Listening carefully helps learn faster and avoid mistakes Time management plays a big role because balancing study practice rest and fun is necessary for mental health A healthy routine includes proper sleep exercise and breaks along with study sessions Success is not achieved overnight but through small daily efforts that add up over time Always remember formulas like Practice + Patience = Progress and Effort x Consistency = Success Life rewards those who keep moving forward even after failures because failure is not the end but a lesson that prepares you for future success In the end learning coding and education is not just about jobs money or grades but about becoming a confident skilled and responsible person who can contribute positively to society and build a meaningful life with knowledge skills and determination :)'.split(' ');
+const wordsCount = esy.length;
+const gameTime = 60 * 1000;
+window.timer = null;
+window.gameStart = null;
+window.pauseTime = 0;
+
+function addClass(el,name) {
+  el.className += ' '+name;
+}
+function removeClass(el,name) {
+  el.className = el.className.replace(name,'');
+}
+
+function randomWord() {
+  const randomIndex = Math.ceil(Math.random() * wordsCount);
+  return esy[randomIndex - 1];
+}
+
+function formatWord(esy) {
+  return `<div class="word"><span class="letter">${esy.split('').join('</span><span class="letter">')}</span></div>`;
+}
+
+function newGame() {
+  const game = document.getElementById('game');
+
+  // game state reset
+  game.className = 'game';   // over class clean
+
+  // timer reset
+  clearInterval(window.timer);
+  window.timer = null;
+  window.gameStart = null;
+
+  // words reset
+  const wordsEl = document.getElementById('words');
+  wordsEl.innerHTML = '';
+  wordsEl.style.marginTop = '0px';
+
+  // new words generate
+  for (let i = 0; i < 200; i++) {
+    wordsEl.innerHTML += formatWord(randomWord());
+  }
+
+  // first word & letter active
+  addClass(document.querySelector('.word'), 'current');
+  addClass(document.querySelector('.letter'), 'current');
+
+  // reset timer display
+  document.getElementById('info').innerHTML = (gameTime / 1000) + '';
+
+  // cursor position fix
+  updateCursor();
+}
+
+
+
+function getWpm() {
+  const words = [...document.querySelectorAll('.word')];
+  const lastTypedWord = document.querySelector('.word.current');
+  const lastTypedWordIndex = words.indexOf(lastTypedWord) + 1;
+  const typedWords = words.slice(0, lastTypedWordIndex);
+  const correctWords = typedWords.filter(word => {
+    const letters = [...word.children];
+    const incorrectLetters = letters.filter(letter => letter.className.includes('incorrect'));
+    const correctLetters = letters.filter(letter => letter.className.includes('correct'));
+    return incorrectLetters.length === 0 && correctLetters.length === letters.length;
+  });
+  return correctWords.length / gameTime * 60000;
+}
+
+function gameOver() {
+  clearInterval(window.timer);
+  addClass(document.getElementById('game'), 'over');
+  const result = getWpm();
+  document.getElementById('info').innerHTML = `WPM: ${result}`;
+}
+
+document.getElementById('game').addEventListener('keyup', ev => {
+  const key = ev.key;
+  const currentWord = document.querySelector('.word.current');
+  const currentLetter = document.querySelector('.letter.current');
+  const expected = currentLetter?.innerHTML || ' ';
+  const isLetter = key.length === 1 && key !== ' ';
+  const isSpace = key === ' ';
+  const isBackspace = key === 'Backspace';
+  const isFirstLetter = currentLetter === currentWord.firstChild;
+
+  if (document.querySelector('#game.over')) {
+    return;
+  }
+
+  console.log({key,expected});
+
+  if (!window.timer && isLetter) {
+    window.timer = setInterval(() => {
+      if (!window.gameStart) {
+        window.gameStart = (new Date()).getTime();
+      }
+      const currentTime = (new Date()).getTime();
+      const msPassed = currentTime - window.gameStart;
+      const sPassed = Math.round(msPassed / 1000);
+      const sLeft = Math.round((gameTime / 1000) - sPassed);
+      if (sLeft <= 0) {
+        gameOver();
+        return;
+      }
+      document.getElementById('info').innerHTML = sLeft + '';
+    }, 1000);
+  }
+
+  if (isLetter) {
+  if (currentLetter) {
+    addClass(currentLetter, key === expected ? 'correct' : 'incorrect');
+    removeClass(currentLetter, 'current');
+
+    if (currentLetter.nextSibling) {
+      addClass(currentLetter.nextSibling, 'current');
+    }
+  } else {
+    // ✅ FIX: word complete hone ke baad typing block
+    return;
+  }
+}
+
+
+  if (isSpace) {
+    if (expected !== ' ') {
+      const lettersToInvalidate = [...document.querySelectorAll('.word.current .letter:not(.correct)')];
+      lettersToInvalidate.forEach(letter => {
+        addClass(letter, 'incorrect');
+      });
+    }
+    removeClass(currentWord, 'current');
+    addClass(currentWord.nextSibling, 'current');
+    if (currentLetter) {
+      removeClass(currentLetter, 'current');
+    }
+    addClass(currentWord.nextSibling.firstChild, 'current');
+  }
+
+  if (isBackspace) {
+    if (currentLetter && isFirstLetter) {
+      // make prev word current, last letter current
+      removeClass(currentWord, 'current');
+      addClass(currentWord.previousSibling, 'current');
+      removeClass(currentLetter, 'current');
+      addClass(currentWord.previousSibling.lastChild, 'current');
+      removeClass(currentWord.previousSibling.lastChild, 'incorrect');
+      removeClass(currentWord.previousSibling.lastChild, 'correct');
+    }
+    if (currentLetter && !isFirstLetter) {
+      // move back one letter, invalidate letter
+      removeClass(currentLetter, 'current');
+      addClass(currentLetter.previousSibling, 'current');
+      removeClass(currentLetter.previousSibling, 'incorrect');
+      removeClass(currentLetter.previousSibling, 'correct');
+    }
+    if (!currentLetter) {
+      addClass(currentWord.lastChild, 'current');
+      removeClass(currentWord.lastChild, 'incorrect');
+      removeClass(currentWord.lastChild, 'correct');
+    }
+  }
+
+  // move lines / words
+  if (currentWord.getBoundingClientRect().top > 250) {
+    const words = document.getElementById('words');
+    const margin = parseInt(words.style.marginTop || '0px');
+    words.style.marginTop = (margin - 35) + 'px';
+  }
+
+  // move cursor
+  const nextLetter = document.querySelector('.letter.current');
+  const nextWord = document.querySelector('.word.current');
+  const cursor = document.getElementById('cursor');
+  cursor.style.top = (nextLetter || nextWord).getBoundingClientRect().top + 2 + 'px';
+  cursor.style.left = (nextLetter || nextWord).getBoundingClientRect()[nextLetter ? 'left' : 'right'] + 'px';
+});
+
+document.getElementById('newGameBtn').addEventListener('click', () => {
+  newGame();
+  document.getElementById('game').focus(); 
+});
+
+
+newGame();
+document.getElementById('game').focus();
